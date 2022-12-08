3e2420e3894fad6d6d76fccbf7f464594a7fe8c6
m5production <dimikh@mail.ru>
2022-12-08 18:51:20 +0300

    feat: burger and gallery operation in js

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,6 +1,18 @@
-function toggleNav(){
-    const burger = document.querySelector('.burger');
-    const header = document.querySelector('.large-screen-header');
-    burger.classList.toggle('active');
+'use strict'
+
+const burgerBtn = document.querySelector('#burger-btn');
+burgerBtn.addEventListener('click', toggleNav);
+const header = document.querySelector('.large-screen-header');
+const menuWrapper = document.querySelector('.menu-wrapper');
+menuWrapper.onclick = (e) => {
+    if(e.target !== header){
+        toggleNav();
+    }
+    console.log(':)');
+}
+
+function toggleNav() {
+    menuWrapper.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
     header.classList.toggle('active');
 }
--- pages/main/script.js
+++ pages/main/script.js
@@ -1,6 +1,140 @@
-function toggleNav(){
-    const burger = document.querySelector('.burger');
+'use strict'
+
+//============BURGER============
+{
+    const burgerBtn = document.querySelector('#burger-btn');
+    burgerBtn.addEventListener('click', toggleNav);
     const header = document.querySelector('.large-screen-header');
-    burger.classList.toggle('active');
-    header.classList.toggle('active');
+    const menuWrapper = document.querySelector('.menu-wrapper');
+    const body = document.querySelector('body');
+
+    menuWrapper.onclick = (e) => {
+        if (e.target !== header) {
+            toggleNav();
+        }
+        console.log(':)');
+    }
+
+    function toggleNav() {
+        menuWrapper.classList.toggle('active');
+        burgerBtn.classList.toggle('active');
+        header.classList.toggle('active');
+        if (getComputedStyle(body).overflow !== 'hidden') body.style.overflow = 'hidden';
+        else body.style.overflow = '';
+    }
+}
+
+//===========GALLERY============
+
+{
+    const paneContainer = document.querySelector('.cards-collection-container');
+    const cardsPane = document.querySelector('.animal-cards.active-pane');
+    paneContainer.style.height = cardsPane.offsetHeight + 'px';
+    const cardsCollection = cardsPane.children;
+    const collectionLength = cardsCollection.length;
+    let slidingEnabled = true;
+
+    let numberOfVisibleItems = getNumberOfVisibleItems();
+    let firstVisibleIndex = 0;
+    let lastVisibleIndex = numberOfVisibleItems - 1;
+
+    paneContainer.prepend(makePrev());
+    paneContainer.append(makeNext());
+
+    function getNumberOfVisibleItems() {
+        const browserWidth = document.documentElement.clientWidth;
+        return browserWidth > 640 ? 6
+            : browserWidth > 320 ? 4
+                : 1;
+    }
+
+    //Pane generation
+    function makeNext() {
+        const firstIndex = lastVisibleIndex + 1;
+        const nextPane = document.createElement('ul');
+        nextPane.classList.add('animal-cards', 'next-pane');
+
+        for (let i = 0; i < numberOfVisibleItems; i++) {
+            const nextElem = cardsCollection[(i + firstIndex) % collectionLength];
+            nextPane.append(nextElem.cloneNode(true));
+        }
+
+        return nextPane;
+    }
+
+    function makePrev() {
+        const firstIndex = (collectionLength + firstVisibleIndex - numberOfVisibleItems) % collectionLength;
+        const prevPane = document.createElement('ul');
+        prevPane.classList.add('animal-cards', 'prev-pane');
+
+
+        for (let i = 0; i < numberOfVisibleItems; i++) {
+            const nextElem = cardsCollection[(firstIndex + i) % collectionLength];
+            prevPane.append(nextElem.cloneNode(true));
+        }
+
+        return prevPane;
+    }
+
+    //=========TESTS=================
+    // paneContainer.innerHTML = '';
+    // const nextPane = makePrev();
+    // nextPane.classList.add('animal-cards');
+    // paneContainer.append(nextPane);
+
+
+    //Sliding operations
+    function hidePane(direction) {
+        slidingEnabled = false;
+        const oldPaneType =
+        direction === 'to-right'
+            ? 'next-pane'
+            : 'prev-pane';
+        const activePane = document.querySelector('.animal-cards.active-pane');
+        activePane.classList.add(direction);
+        activePane.addEventListener('animationend', function () {
+            console.log('работает');
+            this.className = `animal-cards ${oldPaneType}`;
+        })
+    }
+
+    function showPane(direction) {
+        const newPaneType =
+            direction === 'from-right'
+                ? 'next-pane'
+                : 'prev-pane';
+        const nextPane = document.querySelector(`.animal-cards.${newPaneType}`);
+        nextPane.classList.add(direction);
+        nextPane.addEventListener('animationend', function () {
+            this.className = 'animal-cards active-pane';
+            slidingEnabled = true;
+        })
+    }
+
+    function slideNext() {
+        const prevPane = document.querySelector('.animal-cards.prev-pane');
+        prevPane.remove();
+        hidePane('to-left');
+        showPane('from-right');
+        firstVisibleIndex = (lastVisibleIndex + 1) % collectionLength;
+        lastVisibleIndex = (lastVisibleIndex + numberOfVisibleItems) % collectionLength;
+        paneContainer.append(makeNext());
+    }
+
+    function slidePrev() {
+        const nextPane = document.querySelector('.animal-cards.next-pane');
+        nextPane.remove();
+        hidePane('to-right');
+        showPane('from-left');
+        lastVisibleIndex = (collectionLength + firstVisibleIndex - 1) % collectionLength;
+        firstVisibleIndex = (collectionLength + firstVisibleIndex - numberOfVisibleItems) % collectionLength;
+        paneContainer.prepend(makePrev());
+    }
+
+    //Buttons
+    const next = document.querySelector('#gallery-btn-next');
+    const prev = document.querySelector('#gallery-btn-prev');
+
+    next.addEventListener('click', slideNext);
+    prev.addEventListener('click', slidePrev);
 }
