349b82b121aa2d6e67b51b39172ebf2631d4e2b4
m5production <dimikh@mail.ru>
2022-12-13 12:25:14 +0300

    fix: pick-out burger func into separate file

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,28 +1,4 @@
 'use strict'
-
-//============BURGER============
-{
-    const burgerBtn = document.getElementById('burger-btn');
-    burgerBtn.addEventListener('click', toggleNav);
-    const header = document.querySelector('.large-screen-header');
-    const menuWrapper = document.querySelector('.menu-wrapper');
-    const body = document.body;
-
-    menuWrapper.addEventListener('click', (e) => {
-        if (!header.contains(e.target)) {
-            toggleNav();
-        }
-    });
-
-    function toggleNav() {
-        menuWrapper.classList.toggle('active');
-        burgerBtn.classList.toggle('active');
-        header.classList.toggle('active');
-        if (getComputedStyle(body).overflow !== 'hidden') body.style.overflow = 'hidden';
-        else body.style.overflow = '';
-    }
-}
-
 //=========DONATE-SUMS==========
 
 {
